4bb65876be455007332aae378449704859b338d9
ilpropheta <marcoarena@email.it>
2018-05-01 17:03:15 +0200

    ex 2

--- cpp-project/microurl/src/ver2/tests/MicroUrlInitializationTest.cpp
+++ cpp-project/microurl/src/ver2/tests/MicroUrlInitializationTest.cpp
@@ -2,9 +2,9 @@
 #include "ver1/DbIdGenerator.h"
 #include "ver1/MicroUrlService.h"
 
-TEST_CASE("Initialization Test", "Can you make this compile? [MakeUrl]",)
+TEST_CASE("Initialization Test", "Can you turn DbIdGenerator* into a unique_ptr? [MakeUrl]",)
 {
-	MicroUrlService service(std::make_unique<DbIdGenerator>());
+	MicroUrlService service(new DbIdGenerator>());
 	auto microUrl = service.MakeMicroUrl("http://google.com");
 	REQUIRE(microUrl == "https://micro.url/bM");
 }
