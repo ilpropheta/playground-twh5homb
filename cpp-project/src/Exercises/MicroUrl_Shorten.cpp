65d51e1a6bff967f66dec43221639d5ba7cbbaaf
ilpropheta <marcoarena@email.it>
2018-05-01 12:55:12 +0200

    removed useless stuff

--- cpp-project/src/Exercises/MicroUrl_Shorten.cpp
+++
@@ -1,6 +0,0 @@
-#include "MicroUrl.h"
-
-std::string MicroUrl::Shorten(const char* url)
-{
-  return url;
-}
--- cpp-project/src/Exercises/MicroUrl_Visit.cpp
+++
@@ -1,6 +0,0 @@
-#include "MicroUrl.h"
-
-std::string MicroUrl::Visit(const char* url)
-{
-  return url;
-}
